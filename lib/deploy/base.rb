624ad6d15ee661ed6cbaa17f34b7b70bddf21f21
Philip MacIver <philip.maciver@xing.com>
2011-07-28 18:37:50 +0200

    Added sanity the deploy. I must have been on crack before

--- lib/deploy/base.rb
+++ lib/deploy/base.rb
@@ -14,37 +14,52 @@ module Deploy
       self.commands << [:local, command]
     end
 
+    def ssh_cmd(commands)
+      cmd = "ssh "
+      cmd << "#{dep_config.get(:extra_ssh_options)} " if dep_config.get(:extra_ssh_options)
+      cmd << "#{dep_config.get(:username)}@#{dep_config.get(:remote)} "
+      cmd << "'"
+      cmd << "#{dep_config.get(:after_login)}; " if dep_config.get(:after_login)
+      cmd << "#{commands}"
+      cmd << "'"
+    end
+
     def run_now!(command)
       puts "EXECUTING: #{command}" if dep_config.get(:verbose)
       system command unless dep_config.get(:dry_run)
     end
 
-    def push!
+    def push!(push_now = false)
       unless self.commands.empty?
-        all_commands = self.commands.map do |command|
+        local_commands = []
+        remote_commands = []
+
+        #TODO: Need tests to make sure local and remote work the way they are supposed to
+        self.commands.each do |command|
           if command.first == :local
             puts "LOCAL: #{command.last}" if dep_config.get(:verbose)
-            eval command.last
-            nil
+            local_commands << command.last
           elsif command.first == :remote
             puts "REMOTE: #{command.last}" if dep_config.get(:verbose)
-            command.last
+            remote_commands << command.last
           end
         end
 
-        all_commands = all_commands.compact.join("; ")
+        unless local_commands.empty?
+          run_now! local_commands.join("; ")
+        end
 
-        cmd = "ssh "
-        cmd << "#{dep_config.get(:extra_ssh_options)} " if dep_config.get(:extra_ssh_options)
-        cmd << "#{dep_config.get(:username)}@#{dep_config.get(:remote)} "
-        cmd << "'"
-        cmd << "#{dep_config.get(:after_login)}; " if dep_config.get(:after_login)
-        cmd << "#{all_commands}"
-        cmd << "'"
-        run_now! cmd
+        unless remote_commands.empty?
+         run_now! ssh_cmd(remote_commands.join("; "))
+        end
+
         puts "\n" if dep_config.get(:env) != 'test'
         self.commands = []
       end
+    end
+
+    def big_push!
+      # TODO: The future of pushing stuff to the server
     end
   end
 end
