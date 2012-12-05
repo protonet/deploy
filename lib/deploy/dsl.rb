c32df338d1689b458b9934027efefca7a72bd590
Philip MacIver <philip@ivercore.com>
2012-12-05 17:21:45 +0100

    Convert symbols to strings before trying to compare them

--- lib/deploy/dsl.rb
+++ lib/deploy/dsl.rb
@@ -15,7 +15,7 @@ module Deploy
         end
 
         def self.all_descriptions
-          self.descriptions.sort{|a,b| a.first <=> b.first}
+          self.descriptions.sort{|a,b| a.to_a.first <=> b.to_a.first}
         end
 
         def self.prepend(action, prepend_before = nil)
