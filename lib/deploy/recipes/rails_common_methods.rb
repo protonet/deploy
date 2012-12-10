c21f32bac915ed0cacbedca9c768175a1e0f7de5
Philip MacIver <philip@ivercore.com>
2012-12-10 10:43:36 +0100

    Allow options in bundler

--- lib/deploy/recipes/rails_common_methods.rb
+++ lib/deploy/recipes/rails_common_methods.rb
@@ -12,7 +12,18 @@ module Deploy
 
           desc :bundle, "Runs bundle to make sure all the required gems are on the ststem" do
             remote "cd #{dep_config.app_root}"
-            remote "RAILS_ENV=#{dep_config.env} bundle install --without test development --deployment"
+
+            cmd = []
+            cmd << "RAILS_ENV=#{dep_config.env}"
+            cmd << "bundle install"
+            cmd << "--without test development"
+            cmd << "--deployment"
+
+            if dep_config.exists?(:bundle_bin_stubs) && dep_config.bundle_bin_stubs
+              cmd << "--binstubs"
+            end
+
+            remote cmd.join(' ')
           end
 
           def self.rake(command)
