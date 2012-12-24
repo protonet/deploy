736225ab961a76ae7fe88b1b4f01ac08c0d2c4e7
Philip MacIver <philip@ivercore.com>
2012-12-24 08:56:26 +0100

    Logic error

--- lib/deploy/recipes/rails_methods.rb
+++ lib/deploy/recipes/rails_methods.rb
@@ -33,7 +33,7 @@ module Deploy
             cmd << "--without test development"
             cmd << "--deployment"
 
-            if dep_config.exists?(:bundle_binstubs) && dep_config.bundle_binstubs
+            if dep_config.exists?(:bundler_binstubs) && dep_config.bundle_binstubs
               cmd << "--binstubs"
             end
 
